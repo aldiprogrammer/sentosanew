4832201131ce73ea8b249b0efc9e853528ff4529
aldiwebprogreming <davinal1956@gmail.com>
2026-06-06 15:07:30 +0700

    update

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -49,14 +49,14 @@ export default function AdminLayout({ children }) {
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                                 </svg>
-                                <span className="badge badge-sm badge-success indicator-item">3</span>
+                                <span className="badge badge-sm badge-success indicator-item">0</span>
                             </div>
                         </button>
 
                         <div className="dropdown dropdown-end">
                             <div tabindex="0" role="button" className="btn btn-ghost flex items-center gap-2">
                                 <div className="avatar placeholder">
-                                    <div className="bg-primaryGreen text-white rounded-full w-10">
+                                    <div className="bg-green-700 text-white rounded-full w-10">
                                         <span>{getInitial(auth?.user?.username)}</span>
                                     </div>
                                 </div>
@@ -65,9 +65,9 @@ export default function AdminLayout({ children }) {
 
                             <ul tabindex="0"
                                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                <li>
+                                {/* <li>
                                     <Link href={route('profile.edit')}>Profile</Link>
-                                </li>
+                                </li> */}
                                 <li>
                                     <button onClick={handleLogout} className="text-error w-full text-left">
                                         Logout
